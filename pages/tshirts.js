4dd3a50dee3402d1a3208dd785b17c38c7930271
Rohit <rk212300@outlook.com>
2023-01-04 19:11:22 +0530

    Fetching Tshirts from DB

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,190 +1,38 @@
 import React from "react";
 import Link from "next/link";
-const Tshirts = () => {
+import Product from "../models/Product";
+import mongoose from "mongoose";
+const Tshirts = ({ products }) => {
     return (
         <div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap justify-center -m-4">
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link href="/product/wear-the-code">
-                            <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
-                                <a className="block relative rounded overflow-hidden">
-                                    <img
-                                        alt="ecommerce"
-                                        className="h-[30vh] m-auto md:h-[30vh]"
-                                        src="https://m.media-amazon.com/images/I/61qta+0D54L._AC_UX522_.jpg"
-                                    />
-                                </a>
-                                <div className="mt-4 text-center md:text-left">
-                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                        T-Shirts
-                                    </h3>
-                                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                                        Wear The Code
-                                    </h2>
-                                    <p className="mt-1">Rs 499</p>
-                                    <p className="mt-1">S,M,L,XL</p>
-                                </div>
-                            </div>
-                        </Link>
+                        {
+                            products.map((product) => {
+                                return <Link key={product._id} href={`product/${product.slug}`}>
+                                    <div className="p-4 w-full cursor-pointer shadow-lg m-1 rounded-md">
+                                        <a className="block relative rounded overflow-hidden">
+                                            <img
+                                                alt="ecommerce"
+                                                className="h-[30vh] m-auto md:h-[36vh] block"
+                                                src={product.image}
+                                            />
+                                        </a>
+                                        <div className="mt-4 text-center md:text-left">
+                                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                                                {product.category}
+                                            </h3>
+                                            <h2 className="text-gray-900 title-font text-lg font-medium">
+                                                {product.title}
+                                            </h2>
+                                            <p className="mt-1">Rs {product.price}</p>
+                                            <p className="mt-1">{product.size}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            })
+                        }
 
                     </div>
                 </div>
@@ -193,4 +41,19 @@ const Tshirts = () => {
     );
 };
 
+// server side rendering
+export async function getServerSideProps(context) {
+    if (!mongoose.connections[0].readyState) {
+        // make connection to mongodb
+        await mongoose.connect(process.env.MONGO_URI)
+    }
+    let products = await Product.find({ category: "tshirt" });
+
+    return {
+        props: {
+            products: JSON.parse(JSON.stringify(products))
+        }
+    }
+
+}
 export default Tshirts;
